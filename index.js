65c0183c1f9048935575423cb510d5890099335e
Geric <gericmorit3211@gmail.com>
2026-03-08 23:35:13 +0800

    added admin routes

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use('/auth', require('./routes/auth'));
 app.use('/user', require('./routes/user'));
 app.use('/thesis', require('./routes/thesis'));
+app.use('/admin', require('./routes/admin'));
 
 // Start server
 app.listen(PORT, () => {
